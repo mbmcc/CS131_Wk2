399a9a08b6d0967115d863e343628350b7d10cf6
mbmcc <m.mccourry@gmail.com>
2018-04-15 23:17:33 -0700

    Changes to be committed: 	modified:   Invoice.h
     Untracked files:
    	../.db_wk2.h.swp
    	.hardware_invoice.cpp.swp
    	hardware_invoice.cpp

--- hw_Assignments/Invoice.h
+++ hw_Assignments/Invoice.h
@@ -1,28 +1,24 @@
-#ifndef VARIABLE
-#define VARIABLE
-// Write your header file here.
-#endif
-*******************************************************************************/
-/*  ###############  CS131_wk2_McCourry Invoice Class ##################
-(1) Create a class called Invoice that a hardware store might use to represent an invoice
-for an item sold at the store. An Invoice should include four data members—
- + a part number (type string), 
- + a part description  (type string), 
- + a quantity of the item being purchased ( type int) 
- + and a price per item (type int). 
-(2) Your class should have a constructor that initializes the four data members. 
-A constructor that receives multiple arguments is defined with the 
-form: ClassName( TypeName1 parameterName1, TypeName2 parameterName2, ... ) 
-(3) Provide a set and a get function for each data member.
-(4) In addition, provide a member function named getInvoiceAmount that calculates 
-the invoice amount (i. e., multiplies the quantity by the price per item), 
-then returns the amount as an int value. 
-If the quantity is not positive, 
- it should be set to 0. 
-If the price per item is not positive, 
- it should be set to 0.
-   ############################################################################
-*/
+///*******************************************************************************/
+///*  ###############  CS131_wk2_McCourry Invoice Class ##################
+//(1) Create a class called Invoice that a hardware store might use to represent an invoice
+//for an item sold at the store. An Invoice should include four data members—
+// + a part number (type string), 
+// + a part description  (type string), 
+// + a quantity of the item being purchased ( type int) 
+// + and a price per item (type int). 
+//(2) Your class should have a constructor that initializes the four data members. 
+//A constructor that receives multiple arguments is defined with the 
+//form: ClassName( TypeName1 parameterName1, TypeName2 parameterName2, ... ) 
+//(3) Provide a set and a get function for each data member.
+//(4) In addition, provide a member function named getInvoiceAmount that calculates 
+//the invoice amount (i. e., multiplies the quantity by the price per item), 
+//then returns the amount as an int value. 
+//If the quantity is not positive, 
+// it should be set to 0. 
+//If the price per item is not positive, 
+// it should be set to 0.
+//   ############################################################################
+//*/
 // Invoice.h
 // Class definition separate from the main
 #include <iostream>
@@ -48,7 +44,7 @@ class Invoice
     // getPartNumber
     int getPartNumber() const
     {
-      return parNumber; //display part number
+      return partNumber; //display part number
     }  
     
     // setPartDescription
