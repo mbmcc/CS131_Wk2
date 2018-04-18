cba662bd92593ab61e584eaaf5b5e8b70d9c9e03
mbmcc <m.mccourry@gmail.com>
2018-04-17 22:32:21 -0700

    more attempts at getting the class file to work

--- hw_Assignments/Invoice.h
+++ hw_Assignments/Invoice.h
@@ -1,22 +1,22 @@
-///*******************************************************************************/
-///*  ###############  CS131_wk2_McCourry Invoice Class ##################
-//(1) Create a class called Invoice that a hardware store might use to represent an invoice
-//for an item sold at the store. An Invoice should include four data members—
-// + a part number (type string), 
-// + a part description  (type string), 
-// + a quantity of the item being purchased ( type int) 
-// + and a price per item (type int). 
-//(2) Your class should have a constructor that initializes the four data members. 
-//A constructor that receives multiple arguments is defined with the 
-//form: ClassName( TypeName1 parameterName1, TypeName2 parameterName2, ... ) 
-//(3) Provide a set and a get function for each data member.
-//(4) In addition, provide a member function named getInvoiceAmount that calculates 
-//the invoice amount (i. e., multiplies the quantity by the price per item), 
-//then returns the amount as an int value. 
-//If the quantity is not positive, 
-// it should be set to 0. 
-//If the price per item is not positive, 
-// it should be set to 0.
+//*******************************************************************************/
+//*  ###############  CS131_wk2_McCourry Invoice Class ##################
+// 
+// 
+// (1) Create a class called Invoice that a hardware store might use to represent an invoice
+// for an item sold at the store. An Invoice should include four data members—
+//  + a part number (type string), 
+//  + a part description  (type string), 
+//  + a quantity of the item being purchased ( type int) 
+//  + and a price per item (type int). 
+// (2) Your class should have a constructor that initializes the four data members. 
+// A constructor that receives multiple arguments is defined with the 
+// form: ClassName( TypeName1 parameterName1, TypeName2 parameterName2, ... ) 
+// (3) Provide a set and a get function for each data member.
+// (4) In addition, provide a member function named getInvoiceAmount that calculates 
+// the invoice amount (i. e., multiplies the quantity by the price per item), 
+// then returns the amount as an int value. 
+// If the quantity is not positive, it should be set to 0. 
+// If the price per item is not positive, it should be set to 0.
 //   ############################################################################
 //*/
 // Invoice.h
@@ -25,17 +25,23 @@
 #include <string>
 // Invoice class definition // Item (1)
 
-class Invoice
-{
+class Invoice {
+
   public:
     //constructor // Item (2)
-    Invoice( int number, std::string description, int qty, int price )
-    : partNumber( number ), partDescription( description ), 
-      partQuantaty( qty ), partPrice( price )
+    Invoice(int number, std::string description, int qty, int price) : partNumber(number), partDescription(description), partQuantaty(qty), partPrice(price)
     {     //Do stuff constructor functions
-    }     //Empty function
+    };    //Empty functaion
     // Class functions // Item (3)
-    
+   private:
+    // variable definition
+    int number; 
+    std::string description; 
+    int qty; 
+    int price;   
+  
+    // Member function definitions
+    //
     // setPartNumber
     void setPartNumber( int number )
     {
@@ -50,13 +56,13 @@ class Invoice
     // setPartDescription
     void setPartDescription( std::string description )
     {	
-		  partDescription( = description; //store description in partDescription
+		  partDescription = description; //store description in partDescription
     }
     
     // getPartDescription
     std::string getPartDescription() const
     {
-      return partDescription; //displays the part description string
+      return partDescription; //displays the part description std::string
     }
 
     // setpartQuantaty
@@ -84,14 +90,9 @@ class Invoice
     }
 
     // Get Invoice amount // Item (4)
-    int getInvoiceAmount(partPrice, partQuantaty)
+    int getInvoiceAmount(int partPrice, int partQuantaty)
     {
       return partPrice * partQuantaty; //total price
     }
-  private:
-    // variable definition
-    int number; 
-    std::string description; 
-    int number; 
-    int price;
+
 };
---
+++ hw_Assignments/hardware_invoice.cpp
@@ -0,0 +1,26 @@
+// hardware invoice to test Invoice.h
+//
+// including the Invoice.h class
+
+#include <iostream>
+#include "Invoice.h" 
+
+using namespace std;
+
+//begin execution
+int main()
+{
+	//invoice object contains : 
+	//getPartNumber, getPartDescription, getPartQuantaty, getPartPrice
+  
+  Invoice invoice1( 1001, "5x5 printed circuit card", 7, 15 );
+
+  cout << "The part number is: " << Invoice.getPartNumber() << "\n"\
+       << "The description is: " << Invoice.getPartDescription() << "\n"\
+       << "The quantity is: " << Invoice.getPartQuantaty() << "\n"\
+       << "The price per unit is: " << Invoice.getPartPrice() << "\n"\
+       << "The total cost is: " << Invoice.getInvoiceAmount() << "\n"\
+       ;
+
+}
+
